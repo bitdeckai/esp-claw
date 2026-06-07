1bcb9fa48b8a16aeb5b6629c4f71f70b1b356c6b
bitdeckai <justinleebitdeckai@gmail.com>
2026-06-08 02:23:41 +0800

    时间显示问题

--- components/common/emote/emote.c
+++ components/common/emote/emote.c
@@ -21,6 +21,7 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 #include "gfx.h"
+#include "widget/gfx_font_lvgl.h"
 #include "display_arbiter.h"
 
 static const char *TAG = "app_emote";
@@ -39,6 +40,10 @@ static const char *TAG = "app_emote";
 #define EMOTE_TIME_UPDATE_MS 1000
 #define EMOTE_IM_BOUNCE_STEP_MS 150
 #define EMOTE_IM_BOUNCE_STEPS 6
+#define EMOTE_TIME_LABEL_WIDTH 120
+#define EMOTE_DATE_LABEL_WIDTH 120
+#define EMOTE_TIME_LABEL_HEIGHT 30
+#define EMOTE_DATE_LABEL_HEIGHT 14
 
 #define EMOTE_COLOR_DIM_HEX      0x7A7A7A
 #define EMOTE_COLOR_LLM_ON_HEX   0x4DD488
@@ -68,6 +73,7 @@ typedef struct {
     gfx_obj_t *llm_labels[EMOTE_LLM_COUNT];
     gfx_obj_t *im_labels[EMOTE_IM_COUNT];
     gfx_obj_t *center_logo;
+    gfx_obj_t *date_label;
     gfx_obj_t *clock_label;
     gfx_obj_t *llm_icons[EMOTE_LLM_COUNT];
     gfx_obj_t *im_icons[EMOTE_IM_COUNT];
@@ -82,6 +88,8 @@ typedef struct {
     bool llm_icon_loaded_configured[EMOTE_LLM_COUNT];
     bool im_icon_loaded_configured[EMOTE_IM_COUNT];
     int im_anim_phase;
+    char date_text_cache[16];
+    bool date_text_cached;
     char clock_text_cache[16];
     bool clock_text_cached;
     gfx_coord_t llm_base_x[EMOTE_LLM_COUNT];
@@ -131,6 +139,9 @@ static emote_provider_badges_t s_provider_badges = {
     .active_im_mark_ms = 0,
     .im_anim_phase = 0,
 };
+
+LV_FONT_DECLARE(font_maison_neue_book_12);
+LV_FONT_DECLARE(font_maison_neue_book_26);
 
 extern const void *emote_acquire_data(emote_handle_t handle, const void *data_ref, size_t size, void **output_ptr);
 
@@ -283,40 +294,72 @@ static bool emote_load_logo_to_dsc(const char *icon_name, gfx_image_dsc_t *out_d
 
 static void emote_update_time_label_locked(void)
 {
+    char date_text[16] = "----/--/--";
     char clock_text[16] = "--:--:--";
     time_t now;
     struct tm local_tm = {0};
-    uint16_t w_clock = 0;
-    uint16_t h_clock = 0;
+    gfx_coord_t x_date;
+    gfx_coord_t y_date;
     gfx_coord_t x_clock;
     gfx_coord_t y_clock;
     const int bottom_pad = 3;
+    const int line_gap = 1;
 
-    if (!s_provider_badges.clock_label) {
+    if (!s_provider_badges.date_label && !s_provider_badges.clock_label) {
         return;
     }
 
     now = time(NULL);
     if (now >= EMOTE_MIN_VALID_EPOCH && localtime_r(&now, &local_tm) != NULL) {
+        strftime(date_text, sizeof(date_text), "%Y-%m-%d", &local_tm);
         strftime(clock_text, sizeof(clock_text), "%H:%M:%S", &local_tm);
     }
 
-    if (!s_provider_badges.clock_text_cached || strcmp(s_provider_badges.clock_text_cache, clock_text) != 0) {
+    if (s_provider_badges.date_label &&
+            (!s_provider_badges.date_text_cached || strcmp(s_provider_badges.date_text_cache, date_text) != 0)) {
+        gfx_label_set_text(s_provider_badges.date_label, date_text);
+        strncpy(s_provider_badges.date_text_cache, date_text, sizeof(s_provider_badges.date_text_cache) - 1);
+        s_provider_badges.date_text_cache[sizeof(s_provider_badges.date_text_cache) - 1] = '\0';
+        s_provider_badges.date_text_cached = true;
+    }
+
+    if (s_provider_badges.clock_label &&
+            (!s_provider_badges.clock_text_cached || strcmp(s_provider_badges.clock_text_cache, clock_text) != 0)) {
         gfx_label_set_text(s_provider_badges.clock_label, clock_text);
         strncpy(s_provider_badges.clock_text_cache, clock_text, sizeof(s_provider_badges.clock_text_cache) - 1);
         s_provider_badges.clock_text_cache[sizeof(s_provider_badges.clock_text_cache) - 1] = '\0';
         s_provider_badges.clock_text_cached = true;
     }
 
-    gfx_obj_get_size(s_provider_badges.clock_label, &w_clock, &h_clock);
+    x_date = (gfx_coord_t)((s_lcd_width - EMOTE_DATE_LABEL_WIDTH) / 2);
+    x_clock = (gfx_coord_t)((s_lcd_width - EMOTE_TIME_LABEL_WIDTH) / 2);
+    y_clock = (gfx_coord_t)(s_lcd_height - EMOTE_TIME_LABEL_HEIGHT - bottom_pad);
+    y_date = s_provider_badges.clock_label ?
+             (gfx_coord_t)(y_clock - EMOTE_DATE_LABEL_HEIGHT - line_gap) :
+             (gfx_coord_t)(s_lcd_height - EMOTE_DATE_LABEL_HEIGHT - bottom_pad);
+    if (y_date < 0) {
+        y_date = 0;
+    }
 
-    y_clock = (gfx_coord_t)(s_lcd_height - (int)h_clock - bottom_pad);
-    x_clock = (gfx_coord_t)((s_lcd_width - (int)w_clock) / 2);
+    if (s_provider_badges.date_label) {
+        gfx_obj_set_pos(s_provider_badges.date_label, x_date, y_date);
+        gfx_obj_set_size(s_provider_badges.date_label, EMOTE_DATE_LABEL_WIDTH, EMOTE_DATE_LABEL_HEIGHT);
+        gfx_label_set_text_align(s_provider_badges.date_label, GFX_TEXT_ALIGN_CENTER);
+        gfx_label_set_long_mode(s_provider_badges.date_label, GFX_LABEL_LONG_CLIP);
+        gfx_label_set_color(s_provider_badges.date_label, GFX_COLOR_HEX(0xF2F2F2));
+        gfx_label_set_bg_enable(s_provider_badges.date_label, false);
+        gfx_obj_set_visible(s_provider_badges.date_label, true);
+    }
 
-    gfx_obj_set_pos(s_provider_badges.clock_label, x_clock, y_clock);
-    gfx_label_set_color(s_provider_badges.clock_label, GFX_COLOR_HEX(0xF2F2F2));
-    gfx_label_set_bg_enable(s_provider_badges.clock_label, false);
-    gfx_obj_set_visible(s_provider_badges.clock_label, true);
+    if (s_provider_badges.clock_label) {
+        gfx_obj_set_pos(s_provider_badges.clock_label, x_clock, y_clock);
+        gfx_obj_set_size(s_provider_badges.clock_label, EMOTE_TIME_LABEL_WIDTH, EMOTE_TIME_LABEL_HEIGHT);
+        gfx_label_set_text_align(s_provider_badges.clock_label, GFX_TEXT_ALIGN_CENTER);
+        gfx_label_set_long_mode(s_provider_badges.clock_label, GFX_LABEL_LONG_CLIP);
+        gfx_label_set_color(s_provider_badges.clock_label, GFX_COLOR_HEX(0xF2F2F2));
+        gfx_label_set_bg_enable(s_provider_badges.clock_label, false);
+        gfx_obj_set_visible(s_provider_badges.clock_label, true);
+    }
 }
 
 static void emote_update_provider_badges_locked(void)
@@ -439,6 +482,7 @@ static void emote_create_provider_badges(void)
         "im_wechat_logo", "im_qq_logo", "im_feishu_logo", "im_telegram_logo"
     };
     static const char *center_logo_obj_name = "center_claw_logo";
+    static const char *date_label_obj_name = "bottom_date_label";
     static const char *clock_label_obj_name = "bottom_clock_label";
     static const char *im_texts[EMOTE_IM_COUNT] = {
         "WX", "QQ", "FS", "TG"
@@ -472,8 +516,28 @@ static void emote_create_provider_badges(void)
         gfx_obj_set_visible(s_provider_badges.center_logo, false);
     }
 
+    s_provider_badges.date_label = emote_create_obj_by_type(s_emote_handle, EMOTE_OBJ_TYPE_LABEL, date_label_obj_name);
     s_provider_badges.clock_label = emote_create_obj_by_type(s_emote_handle, EMOTE_OBJ_TYPE_LABEL, clock_label_obj_name);
+    if (!s_provider_badges.date_label) {
+        ESP_LOGW(TAG, "create date label failed: %s", date_label_obj_name);
+    }
+    if (!s_provider_badges.clock_label) {
+        ESP_LOGW(TAG, "create clock label failed: %s", clock_label_obj_name);
+    }
+    if (s_provider_badges.date_label) {
+        gfx_obj_set_size(s_provider_badges.date_label, EMOTE_DATE_LABEL_WIDTH, EMOTE_DATE_LABEL_HEIGHT);
+        gfx_label_set_text_align(s_provider_badges.date_label, GFX_TEXT_ALIGN_CENTER);
+        gfx_label_set_long_mode(s_provider_badges.date_label, GFX_LABEL_LONG_CLIP);
+        gfx_label_set_font(s_provider_badges.date_label, (void *)&font_maison_neue_book_12);
+        gfx_label_set_bg_enable(s_provider_badges.date_label, false);
+        gfx_obj_set_visible(s_provider_badges.date_label, true);
+    }
     if (s_provider_badges.clock_label) {
+        gfx_obj_set_size(s_provider_badges.clock_label, EMOTE_TIME_LABEL_WIDTH, EMOTE_TIME_LABEL_HEIGHT);
+        gfx_label_set_text_align(s_provider_badges.clock_label, GFX_TEXT_ALIGN_CENTER);
+        gfx_label_set_long_mode(s_provider_badges.clock_label, GFX_LABEL_LONG_CLIP);
+        gfx_label_set_font(s_provider_badges.clock_label, (void *)&font_maison_neue_book_26);
+        gfx_label_set_bg_enable(s_provider_badges.clock_label, false);
         gfx_obj_set_visible(s_provider_badges.clock_label, true);
     }
 
